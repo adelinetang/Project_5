4027de8d5e55a16c237ae01a8bdc54f64fa6116c
Jonathan Luu <jonathanluu2923@gmail.com>
2022-05-21 02:14:07 -0700

    updated dunno about DFS

--- MatrixGraph.java
+++ MatrixGraph.java
@@ -1,6 +1,6 @@
 import java.util.*;
  
- public class MatrixGraph{
+public class MatrixGraph{
      ArrayList<Vertex> vertices; 
      private static int matrix [] [];
 
@@ -26,7 +26,7 @@ import java.util.*;
         }
     }//end checkEdge
 
-    void BFS(int start, int numbvertex)
+    void BFS(int start, int numbvertex)         //is this supposed to be the getBreadthTraversal
     {
         boolean[] examined = new boolean[numbvertex];
         Arrays.fill(examined, false);
@@ -53,4 +53,68 @@ import java.util.*;
             }
         }
     }
+
+    void DFS(int start, int numbvertex){
+
+    }
+
+    public print(){
+        System.out.println("The adjacency matrix for the given graph is: ");
+        for(int i=1; i<=9; i++){
+            System.out.print(i + " ");
+        }
+
+        for(int i=1; i<=9; i++){
+            System.out.print(i + " ");
+                for(int j=1; j<=9; j++){
+                    System.out.print(graph.getEdge(i, j) + " ");
+                }
+        }
+    }
+/*
+      System.out.println("The adjacency matrix for the given graph is: ");
+            System.out.print("  ");
+            for (int i = 1; i <= 9; i++)
+                System.out.print(i + " ");
+            System.out.println();
+
+            for (int i = 1; i <= 9; i++)
+            {
+                System.out.print(i + " ");
+                for (int j = 1; j <= 9; j++)
+                    System.out.print(graph.getEdge(i, j) + " ");
+                System.out.println();
+            }
+
+        }  print should look something like this
+*/
+/*
+    public QueueInterface<T> getdepthFirstTraversal(T origin){
+        //assumes graph is not empty
+        resetVertices();
+        QueueInterface<T> traversalOrder = new LinkedQueue<T>();
+        StackInterface<VertexInterface<T>> vertexStack = new LinkedStack<>();
+
+        VertexInterface<T> originVertex = vertices.getValue(origin);
+        originVertex.visit();
+        traversalOrder.enqueue(origin);     //enqueue vertex label
+        vertexStack.push(originVertex);     //enqueue vertex
+
+        while(!VertexStack.isEmpty()){
+            VertexInterface<T> topVertex = vertexStack.peek();
+            VertexInterface<T> nextNeighbor = topVertex.getUnvisitedNeighbor();
+
+            if(nextNeighbor != null){
+                nextNeighbor.visit();
+                traversalOrder.enqueue(nextNeighbor.getLabel());
+                vertexStack.push(nextNeighbor);
+            }else{//All neighbors are visited
+                vertexStack.pop();
+            }
+        }//end while
+
+        return traversalOrder;
+    }//end getDepthFirstTraversal
+
+ */
 }
