6118f98d2389dec5fa04c5dc0d0386352c6b03d1
adelinetang <adelinetang@cpp.edu>
2022-05-20 20:54:39 -0700

    changed to matrix graph

---
+++ Vertex.java
@@ -0,0 +1,8 @@
+public class Vertex{
+
+    String data;
+    Vertex(String data){
+
+    this.data = data;
+    }
+}
--- VertexInterface.java
+++
@@ -1,8 +0,0 @@
-public interface VertexInterface<T>{
-    public void add(T newEntry);
-    public T removeMax();
-    public T getMax();
-    public boolean isEmpty();
-    public int getSize();
-    public void clear();
-}
